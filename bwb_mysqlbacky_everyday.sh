0a1b091f4d9e6f3b5d21fd1150cfd0b3d5e3a434
Jinjie Meng <875302177@qq.com>
2018-06-24 15:43:39 +0800

    v2.0
    （1）修复了导出数据为空的错误。原因是crontab中使用which命令是无法找到mysqldump的路径的，因此妥协地改为字符串，需要自己手动去设置了。
    （2）增强了备份命令的健壮度。增加localhost的选择，增加跳过锁表的选项。

--- bwb_mysqlbacky_everyday.sh
+++ bwb_mysqlbacky_everyday.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-basepath=$(cd `dirname $0`; pwd)         #shell's dir
-datapath=$basepath'/bakmysql'            #shell's datadir
-mdpath=$(which mysqldump) 	         #mysqldump path
+basepath=$(cd `dirname $0`; pwd)         	#shell's dir
+datapath=$basepath'/bakmysql'            	#shell's datadir
+mdpath="/opt/mysql/server-5.6/bin/mysqldump" 	#mysqldump path
 databasename="your database name"
 user="root"
 password="your password"
+hostname="localhost"
 
 if [ "`ls -A ${datapath}`" != "" ]; then
     mv ${datapath}/*.sql  ${datapath}old/ 2>/dev/null
 fi
 Now=$(date +"%Y-%m-%d") 
 File=${datapath}/bakmysql-$Now.sql 
-${mdpath} -u${user} -p${password} ${databasename} > $File 
+${mdpath} --opt -u${user} -p${password} -h${hostname} ${databasename} --skip-lock-tables > $File
 SevenDays=$(date -d -7day  +"%Y-%m-%d") 
 if [ -f ${datapath}'old/bakmysql-$SevenDays.sql' ]; then
     rm -rf ${datapath}'old/bakmysql-$SevenDays.sql' 
